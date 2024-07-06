86227e63fe56861f1a3222d7bb3c28f1955022bb
Rizky28eka <eka28rizky03haryadi04@gmail.com>
2024-07-07 02:46:58 +0700

    add

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,12 @@
+// Importing the SignIn component from the @clerk/nextjs package
 import { SignIn } from "@clerk/nextjs";
 
+// Main Page component
 export default function Page() {
   return (
-    <div className="h-screen flex justify-center items-center">
+    // Setting up the page to occupy the full screen using flexbox
+    <div className='h-screen flex justify-center items-center'>
+      {/* Rendering the SignIn component to allow users to sign in */}
       <SignIn />
     </div>
   );
